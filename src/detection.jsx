904183004f528db7cbc55ffa856f7fa68a39f1fe
342Hanzla <hanzlamuhammad81@gmail.com>
2026-04-29 21:26:32 +0000

    Update

--- src/detection.jsx
+++ src/detection.jsx
@@ -3,12 +3,43 @@ import Webcam from 'react-webcam';
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
-  const [img,setImg]=useState(null)
+  const [img,setImg]=useState(null);
+  const audioRef=useRef(null);
+  const safeRef=useRef(null);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImg(imageSrc); 
+   Detect(imageSrc);
+
+     
   }, [webcamRef]);
+
+  const Detect=useCallback(async (imageSrc) => {
+    const base64Image = imageSrc.split(',')[1];
+     
+    const response = await fetch(
+        `https://serverless.roboflow.com/firedetection-upd0y/1?api_key=${import.meta.env.VITE_ROBOFLOW_KEY}`,
+        {
+            method: "POST",
+            body: base64Image,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }
+    );
+    
+    const data = await response.json();
+    console.log(data);
+    if(data.predictions.length>0){
+      audioRef.current.play();
+    }
+    else
+    {
+      safeRef.current.play();
+
+    }
+}, [webcamRef]);
 
   return (
     <>
@@ -17,10 +48,12 @@ const WebcamCapture = () => {
         ref={webcamRef}
         screenshotFormat="image/jpeg"
       />
+      <audio ref={audioRef} src='/alarm.mp3'/>
+      <audio ref={safeRef} src='/safe.mp3'/>
+     
       {img && <img src={img} alt="captured"/>}
-
-      <button onClick={capture}>Capture photo</button>
-    </>
+<button onClick={capture}>Capture photo</button> 
+    </> 
   );
 };
 export default WebcamCapture
